c105f127a441e3d772f3232e71116eea88794d95
Carlos Waldo Rojas <krlosw9@gmail.com>
2026-01-08 16:17:13 -0500

    Autenticación con better-auth

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '@prisma/client';
+import { nextCookies } from 'better-auth/next-js';
 
 const prisma = new PrismaClient();
 
@@ -8,12 +9,16 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
+  plugins: [nextCookies()]
 });
 
 export type Session = typeof auth.$Infer.Session;
