15326c9eb5084db021e242eab3a3219389d6b1da
Carlos Waldo Rojas <krlosw9@gmail.com>
2026-01-08 20:13:17 -0500

    exportado PrismaClient para ser usado en las implementaciones repository

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -3,7 +3,7 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '@prisma/client';
 import { nextCookies } from 'better-auth/next-js';
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
